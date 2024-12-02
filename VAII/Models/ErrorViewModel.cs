5970a261992537da56e1cba7d0978e53b8226f04
Andrej <ondrasek@stud.uniza.sk>
2024-12-02 18:42:46 +0100

    Add project files.

---
+++ VAII/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VAII.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
